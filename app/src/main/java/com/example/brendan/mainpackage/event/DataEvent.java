950b4b04f3b12f16aee0dfe51b326da379cf2d40
BrendanOswego <bgoldber@oswego.edu>
2017-01-31 18:54:32 -0500

    Added additional API calls and basic logic for @Query tags with Retrofit

---
+++ app/src/main/java/com/example/brendan/mainpackage/event/DataEvent.java
@@ -0,0 +1,8 @@
+package com.example.brendan.mainpackage.event;
+
+/**
+ * Created by brendan on 1/31/17.
+ */
+
+public class DataEvent {
+}
---
+++ app/src/main/java/com/example/brendan/mainpackage/model/DataResults.java
@@ -0,0 +1,8 @@
+package com.example.brendan.mainpackage.model;
+
+/**
+ * Created by brendan on 1/31/17.
+ */
+
+public class DataResults {
+}
